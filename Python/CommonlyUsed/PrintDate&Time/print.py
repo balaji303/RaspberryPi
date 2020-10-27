cfb80eda92e8c9ce7a04bde3a359174a13960c3d
balaji303 <balajivelu303@outlook.com>
2020-10-27 07:52:59 +0530

    Create print.py

---
+++ Python/CommonlyUsed/PrintDate&Time/print.py
@@ -0,0 +1,8 @@
+print("Welcome");
+print("To the \nJungle")
+a=98+2
+print(a,"this is the value of a") 
+#printf("%d",a)
+print("this %s is the value of a" %(a));
+from datetime import datetime
+print(datetime.now().strftime("%Y-%d-%b %H:%M"))
